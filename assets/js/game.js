1169a9acb2ba70a84cff7c7c085b3e53cf4bc66b
Carolyn B <bmathematics@gmail.com>
2022-01-12 10:39:29 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -35,14 +35,17 @@ var fight = function(enemyName){
         if (confirmSkip){
             window.alert(playerName + " has decided to skip this fight. Goodbye!")
             //subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney)
             break;
         }
     }
         //remove enemy's health by subtracting the amount set in the playerAttack variable
         // subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
-        enemyHealth = enemyHealth - playerAttack;
+        //generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
         //log a resulting messge to the console so we know that it worked.
         console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
         //check enemy's health
@@ -54,7 +57,9 @@ var fight = function(enemyName){
                 window.alert(enemyName + " still has " + enemyHealth + " health left.");
             }
                 //subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
-            playerHealth = playerHealth - enemyAttack;
+            var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+            playerHealth = Math.max(0, playerHealth - damage);
             //log a resulting message to the console so we know that it worked.
             console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
             // check player's health
@@ -84,7 +89,12 @@ var fight = function(enemyName){
 
 // else leave shop and return to fight
 
+//function to generate a random numeric value
+var randomNumber = function(min,max){
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
 
+    return value;
+};
 
 
 
@@ -99,11 +109,12 @@ var startGame = function(){
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
         //call fight function with enemy-robot
         var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40,60);
         fight(pickedEnemyName);
 
             //if the player is still alive and we're not at the last enemy in the array
             if (playerHealth > 0 && i < enemyNames.length - 1){
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
                 if (storeConfirm){
                     shop();
                 }
@@ -159,7 +170,7 @@ var shop = function(){
             }
 
             break;
-        case "UPGRADE";
+        case "UPGRADE":
         case "upgrade":
             if(playerMoney >= 7){
             window.alert("Upgrading player's attack by 6 for 7 dollars.");
